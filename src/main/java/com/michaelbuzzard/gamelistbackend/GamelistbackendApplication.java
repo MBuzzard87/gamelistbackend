e37645485c690886cd8950390bea0da270608613
unknown <m.buzzard87@gmail.com>
2021-07-18 06:22:40 -0400

    initial commit

---
+++ src/main/java/com/michaelbuzzard/gamelistbackend/GamelistbackendApplication.java
@@ -0,0 +1,13 @@
+package com.michaelbuzzard.gamelistbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamelistbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamelistbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michaelbuzzard/gamelistbackend/GamelistbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michaelbuzzard.gamelistbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamelistbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

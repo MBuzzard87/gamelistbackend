715cf1ed9ddcabab9878a8ffd30b9f1d60779c52
unknown <m.buzzard87@gmail.com>
2021-07-21 12:52:31 -0400

    bean update

---
+++ src/main/java/com/michaelbuzzard/gamelistbackend/controller/HelloWorldController.java
@@ -0,0 +1,30 @@
+package com.michaelbuzzard.gamelistbackend.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:4200/")
+@RestController
+public class HelloWorldController {
+
+    //Get, URI - /hello-world, method - "Hello World"
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+//        throw new RuntimeException("An error has happened");
+        return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldBeanPath(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello, %s", name));
+    }
+
+
+}

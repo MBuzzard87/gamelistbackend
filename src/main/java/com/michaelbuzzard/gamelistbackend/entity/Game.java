4e666729389d20479a8010c6b282cb6a6588d3e5
unknown <m.buzzard87@gmail.com>
2021-08-04 19:24:04 -0400

    update classes to previous commit

--- src/main/java/com/michaelbuzzard/gamelistbackend/entity/Game.java
+++ src/main/java/com/michaelbuzzard/gamelistbackend/entity/Game.java
@@ -1,84 +1,82 @@
 package com.michaelbuzzard.gamelistbackend.entity;
 
-import sun.util.resources.Bundles;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.util.Objects;
+
 
 
 @Entity
 public class Game {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String username;
     private String name;
     private String console;
-    private boolean isComplete;
+    private String isComplete;
 
 
-    public Game() {
+    public Game(){
     }
 
-    public Game(long id, String username, String name, String console, boolean isComplete) {
-        this.id = id;
-        this.username = username;
-        this.name = name;
-        this.console = console;
-        this.isComplete = isComplete;
+    public Game(long id,String username,String name,String console,String isComplete){
+        this.id=id;
+        this.username=username;
+        this.name=name;
+        this.console=console;
+        this.isComplete=isComplete;
     }
 
-    public Long getId() {
+    public Long getId(){
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setId(Long id){
+        this.id=id;
     }
 
-    public String getUsername() { return username;}
+    public String getUsername(){return username;}
 
-    public void setUsername(String username) { this.username = username;}
+    public void setUsername(String username){this.username=username;}
 
-    public String getName() {
+    public String getName(){
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setName(String name){
+        this.name=name;
     }
 
-    public String getConsole() {
+    public String getConsole(){
         return console;
     }
 
-    public void setConsole(String console) {
-        this.console = console;
+    public void setConsole(String console){
+        this.console=console;
     }
 
-    public boolean getIsComplete() {
+    public String getIsComplete(){
         return isComplete;
     }
 
-    public void setIsComplete(boolean complete) {
-        isComplete = complete;
-    }
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Game game = (Game) o;
-        return isComplete == game.isComplete && id.equals(game.id) && username.equals(game.username) && name.equals(game.name) && console.equals(game.console);
+    public void setIsComplete(String complete){
+        isComplete=complete;
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hash(id, username, name, console, isComplete);
+    public String toString(){
+        return"Game{"+
+                "id="+id+
+                ",username='"+username+'\''+
+                ",name='"+name+'\''+
+                ",console='"+console+'\''+
+                ",isComplete="+isComplete+
+                '}';
     }
+
+
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,22 @@
-insert into GAME
-values(1,"Buzzywuzzy87", "Little Samson", "NES", TRUE);
+
+DROP TABLE IF EXISTS game_list;
+
+CREATE TABLE game_list
+(
+    id int AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(250) NOT NULL,
+    name VARCHAR(250) NOT NULL,
+    console VARCHAR(250) NOT NULL,
+    is_complete VARCHAR(250) NOT NULL
+
+);
+
+
+INSERT INTO game_list (username,name,console,is_complete)VALUES
+('Buzzywuzzy87','LittleSamson','NES','Yes'),
+('Buzzywuzzy87','SuperCastlevaniaIV','SNES','Yes'),
+('Buzzywuzzy87','SuperMetroid','SNES','Yes'),
+('Buzzywuzzy87','MegaMan','NES','Yes'),
+('Buzzywuzzy87','SonictheHedgehog','Genesis','Yes'),
+('Buzzywuzzy87','CrusaderofCenty','Genesis','No'),
+('Buzzywuzzy87','Castlevania:SymphonyoftheNight','Playstation1','No');

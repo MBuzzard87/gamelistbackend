b8738a86962a073bbcee8e4661a8e3e4fb2a1c91
unknown <m.buzzard87@gmail.com>
2021-08-03 23:09:29 -0400

    update bean

--- src/main/java/com/michaelbuzzard/gamelistbackend/entity/Game.java
+++ src/main/java/com/michaelbuzzard/gamelistbackend/entity/Game.java
@@ -1,7 +1,5 @@
 package com.michaelbuzzard.gamelistbackend.entity;
 
-import sun.util.resources.Bundles;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
